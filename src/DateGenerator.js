71c0111582ebe837420faede37c883ab1194c52e
Mark Pearl <markpearl@gmail.com>
2017-02-07 07:12:11 +1300

    Added chai test

--- src/DateGenerator.js
+++ src/DateGenerator.js
@@ -1,12 +1,8 @@
 var moment = require('moment');
 
 module.exports = {
-    doSomething: function() {
-        var firstDayOfYear = moment("2016-01-01");
-        var daysBack = (firstDayOfYear.weekday() * -1);
-        var start = firstDayOfYear.day(daysBack);
-        console.log(daysBack);
-        console.log(start.format());
-        return -1;
+    startOfYear: function() {
+        var start = moment("2016-01-01").day(1);
+        return start.date();
     }
 }
--- test/setup.spec.js
+++ test/setup.spec.js
@@ -1,8 +1,9 @@
 var assert = require('assert');
+var expect = require('chai').expect
 var dateGenerator = require("../src/DateGenerator.js");
 
-describe('DateGenerator', function() {
-    it('should start on the 1st day of the year', function() {
-        assert.equaldateGenerator.doSomething(), -1);
+describe('In 2016', function() {
+    it('the first Monday of the first week of the year is', function() {
+        expect(dateGenerator.startOfYear()).to.equal(28);
     });
 });
