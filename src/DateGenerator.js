05ff07e8b60e4de2d57cf2f41a0736f2d1fea5f1
Mark Pearl <markpearl@gmail.com>
2017-02-07 19:31:32 +1300

    Make years dynamic

--- src/DateGenerator.js
+++ src/DateGenerator.js
@@ -4,10 +4,14 @@ module.exports = class Person {
 
     constructor(year = 2016) {
        this._year = year.toString();
+       this._startOfYear = moment(this._year + "-01-01") .clone().day(1);
     }
 
     startOfYear() {
-        let start = moment(this._year + "-01-01") .clone().day(1);
-        return start.date();
+        return this._startOfYear.clone().date();
+    }
+
+    weekOf(int weekNum) {
+        
     }
 }
