9c284c2b72fb2f41caa574a8f27043321a59a5e4
Mark Pearl <markpearl@gmail.com>
2017-02-07 18:45:08 +1300

    ...

--- src/DateGenerator.js
+++ src/DateGenerator.js
@@ -1,8 +1,10 @@
 var moment = require('moment');
 
 module.exports = {
+    
+
     startOfYear: function() {
-        var start = moment("2016-01-01").day(1);
+        var start = moment("2016-01-01") .clone().day(1);
         return start.date();
     }
 }
--- test/setup.spec.js
+++ test/setup.spec.js
@@ -3,7 +3,34 @@ var expect = require('chai').expect
 var dateGenerator = require("../src/DateGenerator.js");
 
 describe('In 2016', function() {
-    it('the first Monday of the first week of the year is', function() {
+    it('the first Monday of the first week of 2016 is the 28th of Dec 2015', function() {
         expect(dateGenerator.startOfYear()).to.equal(28);
     });
 });
+
+describe('In 2015', function() {
+    it('the first Monday of the first week of 2015 is the 28th of Dec 2015', function() {
+        expect(dateGenerator.startOfYear()).to.equal(28);
+    });
+});
+
+
+/* 
+
+2016
+Mon 28 Dec 2015
+Tue 29 Dec 2015
+Wed 30 Dec 2015
+Thu 31 Dec 2015
+Fri 1 Jan 2016
+Sat 2
+Sun 3
+Mon 4
+Mon 11
+Mon 25
+
+
+2017
+Mon 2 Jan 2017
+Mon 9 Jan 2017
+*/
