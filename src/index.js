769adfae1a9347ae4ba1a87b6bdbbbca3286bd37
Mark Pearl <mark.pearl@myob.com>
2017-02-12 17:17:00 +1300

    Update generation for 2017

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-let dateGenerator = require('./FirstDayOfWeekGenerator.js');
+let dateGenerator = require('./YearGenerator.js');
+let generator = new dateGenerator();
+let weeks = generator.getPeriodsOfYearFor(2017);
+
+weeks.map((date,index) => {
+        let startOfWeek = date.format("DD MMM YYYY");
+        let endOfWeek = date.clone().add(4, "days").format("DD MMM YYYY");
+        console.log("Week " + index.toString() + " - " + startOfWeek + " to " + endOfWeek );
+    });
