48f7257bbb7acc8b1726d7fad3578ea63873e8fb
Mark Pearl <mark.pearl@myob.com>
2017-02-12 17:29:46 +1300

    ...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 let dateGenerator = require('./YearGenerator.js');
 let generator = new dateGenerator();
 let weeks = generator.getPeriodsOfYearFor(2017);
+let teams = [ 'Galaga', 'Sirius', 'Bumblenauts']
 
 weeks.map((date,index) => {
         let startOfWeek = date.format("DD MMM YYYY");
         let endOfWeek = date.clone().add(4, "days").format("DD MMM YYYY");
-        console.log("Week " + index.toString() + " - " + startOfWeek + " to " + endOfWeek );
+        let team = teams[index % 3];
+        console.log(startOfWeek + " to " + endOfWeek + ", " + team);
     });
